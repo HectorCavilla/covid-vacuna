74dd175852a4f118409672040319ee880692b45b
HectorCavilla <hector.cavilla@gmail.com>
2021-02-12 00:07:57 -0500

    Version 1.0 para MX

--- config/seo.js
+++ config/seo.js
@@ -1,21 +1,21 @@
 // global SEO config
-const title = 'Estado y progreso vacunación COVID-19 España 2021'
+const title = 'Estado y progreso vacunación COVID-19 México 2021'
 const description =
-  'Consulta el estado y progreso de la vacunación del COVID-19 de forma diaria según datos del gobierno.'
+  'Consulta el estado y progreso de la vacunación del COVID-19 de forma diaria según datos del gobierno de México.'
 
 const SEO = {
   title,
   description,
-  canonical: 'https://covid-vacuna.app',
+  canonical: 'https://covid-vacuna-mx.vercel.app/',
   openGraph: {
     type: 'website',
-    locale: 'es_ES',
-    url: 'https://covid-vacuna.app',
+    locale: 'es_MX',
+    url: 'https://covid-vacuna-mx.vercel.app/',
     title,
     description,
     images: [
       {
-        url: 'https://covid-vacuna.app/og.png',
+        url: 'https://covid-vacuna-mx.vercel.app/og.png',
         alt: title,
         width: 1200,
         height: 627
